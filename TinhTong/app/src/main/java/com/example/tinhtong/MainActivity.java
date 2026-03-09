5625b95f3408a572a812fddf9dcc8bf0e1cc1395
Tran Mai Ngoc Duy <tmnduy.it@gmail.com>
2026-03-09 23:26:08 +0700

    feat: fix tinhtong_app

--- TinhTong/app/src/main/java/com/example/tinhtong/MainActivity.java
+++ TinhTong/app/src/main/java/com/example/tinhtong/MainActivity.java
@@ -21,25 +21,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        // ánh xạ ID
-        edtA = findViewById(R.id.edtA);
-        edtB = findViewById(R.id.edtB);
-        btnTong = findViewById(R.id.btnTong);
-        edtKQ = findViewById(R.id.edtKQ);
-        // Sự kiện tính tổng
-        btnTong.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                double a = Double.parseDouble(edtA.getText().toString());
-                double b = Double.parseDouble(edtB.getText().toString());
-                double tong = a + b;
-                edtKQ.setText(String.valueOf(tong));
-            }
-        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    // Sự kiện tính tổng
+    public void XuLyCong(View view){
+        // ánh xạ ID
+        edtA = findViewById(R.id.edtA);
+        edtB = findViewById(R.id.edtB);
+        btnTong = findViewById(R.id.btnTong);
+        edtKQ = findViewById(R.id.edtKQ);
+        try {
+            double a = Double.parseDouble(edtA.getText().toString());
+            double b = Double.parseDouble(edtB.getText().toString());
+            double tong = a + b;
+            edtKQ.setText(String.valueOf(tong));
+        }
+        catch (Exception e){
+            edtKQ.setText("Lỗi nhập liệu!");
+        }
     }
 }
